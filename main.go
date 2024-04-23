4387c2a90e6d4e43b2da8982d326e7bf0b56a656
AlariCode <antonlarichev@gmail.com>
2024-04-23 12:19:26 +0300

    Описание struct

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
 func main() {
 	login := promptData("Введите логин")
 	password := promptData("Введите пароль")
