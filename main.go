0c1890c7491e44ea0b961c448e2baa648eb20905
AlariCode <antonlarichev@gmail.com>
2024-04-24 11:43:52 +0300

    Мутации struct

--- main.go
+++ main.go
@@ -11,26 +11,32 @@ type account struct {
 	url      string
 }
 
-func (acc account) outputPassword() {
-	fmt.Println(acc)
+func (acc *account) outputPassword() {
 	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	acc.password = string(res)
 }
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
 
 func main() {
-	fmt.Println(generatePassword(12))
 	login := promptData("Введите логин")
-	password := promptData("Введите пароль")
+	// password := promptData("Введите пароль")
 	url := promptData("Введите URL")
 
 	myAccount := account{
-		password: password,
-		url:      url,
-		login:    login,
+		url:   url,
+		login: login,
 	}
+	myAccount.generatePassword(12)
 	myAccount.outputPassword()
-
+	fmt.Println(myAccount)
 }
 
 func promptData(prompt string) string {
@@ -38,12 +44,4 @@ func promptData(prompt string) string {
 	var res string
 	fmt.Scan(&res)
 	return res
-}
-
-func generatePassword(n int) string {
-	res := make([]rune, n)
-	for i := range res {
-		res[i] = letterRunes[rand.IntN(len(letterRunes))]
-	}
-	return string(res)
 }
