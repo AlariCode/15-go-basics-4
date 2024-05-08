26fca1c6e16dcb2dd60b2ad459b962857e05d16a
AlariCode <antonlarichev@gmail.com>
2024-05-08 12:18:19 +0300

    Упражнение - Package файлов

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"demo/password/account"
+	"demo/password/files"
 	"fmt"
 )
 
@@ -15,6 +16,7 @@ func main() {
 		return
 	}
 	myAccount.OutputPassword()
+	files.WriteFile()
 	fmt.Println(myAccount)
 }
 
