8341744d31c5948359d4c3004124c88bc73f1f74
AlariCode <antonlarichev@gmail.com>
2024-05-28 17:39:40 +0300

    Анонимные функции

--- main.go
+++ main.go
@@ -40,17 +40,15 @@ Menu:
 
 func findAccount(vault *account.VaultWithDb) {
 	url := promptData([]string{"Введите URL для поиска"})
-	accounts := vault.FindAccounts(url, checkUrl)
+	accounts := vault.FindAccounts(url, func(acc account.Account, str string) bool {
+		return strings.Contains(acc.Url, str)
+	})
 	if len(accounts) == 0 {
 		color.Red("Аккаунтов не найдено")
 	}
 	for _, account := range accounts {
 		account.Output()
 	}
-}
-
-func checkUrl(acc account.Account, str string) bool {
-	return strings.Contains(acc.Url, str)
 }
 
 func deleteAccount(vault *account.VaultWithDb) {
